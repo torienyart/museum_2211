19c30fc646e263b1c8e9c28da96e3247112cd3dd
Tori Enyart <torienyart@gmail.com>
2023-01-03 09:08:57 -0700

    test: Write tests for Exhibit behavior

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -2,5 +2,14 @@ require 'rspec'
 require './lib/exhibit'
 
 RSpec.describe Exhibit do
-  
+  let(:exhibit) {Exhibit.new({name: "Gems and Minerals", cost: 0})}
+
+  it 'exists' do
+    expect(exhibit).to be_an_instance_of(Exhibit)
+  end
+
+  it 'has readable attributes' do
+    expect(exhibit.name).to eq("Gems and Minerals")
+    expect(exhibit.cost).to eq(0)
+  end
 end
