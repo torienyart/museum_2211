d8ea8ca67ca281e65d9373a5110e91ef62d4f797
Tori Enyart <torienyart@gmail.com>
2023-01-03 09:27:51 -0700

    fix: Adjust indentation

--- lib/exhibit.rb
+++ lib/exhibit.rb
@@ -3,7 +3,7 @@ class Exhibit
     attr_reader :name, :cost
 
     def initialize(info)
-        @name = info[:name]
-        @cost = info[:cost]
+			@name = info[:name]
+			@cost = info[:cost]
     end
 end
